ac2e540b92e1d293412a111312a0f8f6472eaa66
Jun-r <348872111@qq.com>
2015-11-03 18:06:26 +0800

    调整公共页面

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
-app.set('views', './views/');
+app.set('views', './views/pages');
 app.set('view engine', 'jade');
-
+// 静态资源请求路径
+var path = require('path');
+app.use(express.static(path.join(__dirname, 'public/')));
 // 首页
 app.get('/', function (req, res) {
     res.render('index', {title:'首页'});
